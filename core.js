544798abc0837042e31b750ab11506c2fb645d84
Andy <9117837+dark-coffee@users.noreply.github.com>
2019-07-28 19:20:41 +0100

    ovrhaul

---
+++ core.js
@@ -0,0 +1,8 @@
+function openSocial() {
+    document.getElementByClass("social-tray").style.width = "250px";
+    alert('done!')
+  }
+  
+function closeSocial() {
+    document.getElementByClass("social-tray").style.width = "0";
+  }
