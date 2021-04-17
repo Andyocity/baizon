65bca1e36ad4c7b253528611424192f1f72e1ca4
Andy <9117837+dark-coffee@users.noreply.github.com>
2021-04-17 18:49:10 +0100

    new site

--- core.js
+++
@@ -1,8 +0,0 @@
-function openSocial() {
-    document.getElementByClass("social-tray").style.width = "250px";
-    alert('done!')
-  }
-  
-function closeSocial() {
-    document.getElementByClass("social-tray").style.width = "0";
-  }
